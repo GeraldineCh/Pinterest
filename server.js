33fb58acb11d797ad006e0a2aba78a44330c009c
Geraldine Chauca <gchaucae@gmail.com>
2017-07-06 10:15:37 -0500

    Header style

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 
 //aquí le decimos que use los archivos estáticos y que se muestren al llamar a la ruta '/'
 app.use('/', express.static('public'));
+app.set('port', (process.env.PORT || 5000));
 
 //aquí le decimos al framework desde que puerto
-app.listen(3000,() => {
-	console.log("Listening on 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
